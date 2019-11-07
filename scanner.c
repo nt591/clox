6c84388a34dcc9622748b2069e972c360b1ada21
Nikhil Thomas <nikhil@arceoanalytics.com>
2019-11-07 10:45:14 -0500

    Scanner ch 3 done wow
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

--- scanner.c
+++ scanner.c
@@ -18,6 +18,16 @@ void initScanner(const char* source) {
   scanner.line = 1;
 }
 
+static bool isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') ||
+    ( c >= 'A' && c <= 'Z') ||
+    c == '_'; //underscores are valid in variables
+}
+
+static bool isDigit(char c) {
+  return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() {
   return *scanner.current == '\0';
 }
@@ -94,6 +104,98 @@ static void skipWhitespace() {
   }
 }
 
+static TokenType checkKeyword(int start, int length, const char* rest, TokenType type) {
+  // if the current position minus start position is equal to start of rest of string and length of rest
+  //   aka hello, scanner.start = 0, current = 5, start = 1, rest = 4
+  // AND if scanner start + string start is the same as the rest of the string
+  // aka Hello, is ello pointing to the same values in the pointer of rest
+  if (scanner.current - scanner.start == start + length &&
+    memcmp(scanner.start + start, rest, length) == 0) {
+      return type;
+    }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+  // https://craftinginterpreters.com/scanning-on-demand.html
+  // first we build a trie, and check if the string is a keyword
+  // easy way, match on first letter of the scanner
+  // if the rest of the letters match a known keyword, return keyword token
+  // else, return identifier token
+
+  switch (scanner.start[0]) {
+    // if the first letter is an A,
+    // check that from position 2, the length of the remaining string is 2
+    // AND that is matches 'nd'
+    case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+    case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+    case 'e': return checkKeyword(1, 3, "else", TOKEN_ELSE);
+    // multiple keywords start with 'f'
+    case 'f':
+      // is there a second letter in this lexeme?
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+          case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+          case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+        }
+      }
+      break;
+    case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+    case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+    case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+    case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+    case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+    case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+    case 't':
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+          case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+        }
+      }
+      break;
+    case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+    case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static Token number() {
+  // look forward while the next char is digit
+  while (isDigit(peek())) advance();
+
+  // now that we're at a non-digit, capture fraction if next char is period
+  if (peek() == '.' && isDigit(peekNext())) {
+    advance();
+    while (isDigit(peek())) advance();
+  } // if we see a period followed by a num, its' a fraction
+
+  return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+  // capture until we see close quote or end of file
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') scanner.line++;
+    advance();
+  }
+
+  if (isAtEnd()) return errorToken("Unterminated string.");
+
+  // close the quote
+  advance();
+  return makeToken(TOKEN_STRING);
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek())) advance();
+
+  return makeToken(identifierType());
+}
+
 Token scanToken() {
   skipWhitespace();
   // we can start at the current pointer, since we know we'll always be at the
@@ -103,6 +205,9 @@ Token scanToken() {
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
   char c = advance();
+  if (isAlpha(c)) return identifier();
+  // just catch any digits out here instead of in switch
+  if (isDigit(c)) return number();
 
   switch(c) {
     case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -124,6 +229,7 @@ Token scanToken() {
       return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
     case '>':
       return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    case '"': return string(); // time to find string literals
   }
 
   return errorToken("Unexpected character.");
