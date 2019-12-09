700dfe02556d706dd208638fc3349b8626e05d70
Nikhil Thomas <nikhil@arceoanalytics.com>
2019-12-09 14:41:28 -0500

    Sorry but I had to forward-declare funDeclaration to compiler
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

--- compiler.c
+++ compiler.c
@@ -55,6 +55,7 @@ typedef enum {
 } FunctionType;
 
 typedef struct Compiler {
+  struct Compiler* enclosing; // every compiler knows which function created it
   ObjFunction* function;
   FunctionType type;
   Local locals[UINT8_COUNT]; // just keep my local variables in the order of declaration
@@ -200,6 +201,8 @@ static void patchJump(int offset) {
 }
 
 static void initCompiler(Compiler* compiler, FunctionType type) {
+  // when we initialize a compiler, capture the current compiler as enclosing
+  compiler->enclosing = current;
   compiler->function = NULL;
   compiler->type = type;
   compiler->localCount = 0;
@@ -212,6 +215,13 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
   */
   compiler->function = newFunction();
   current = compiler;
+
+  // when we init compiler, it comes right after we read the function declaration
+  // which means the previous token is the name of the function
+  if (type != TYPE_SCRIPT) {
+    // copy string since function can outlive a string object in memory
+    current->function->name = copyString(parser.previous.start, parser.previous.length);
+  }
 
   // VM claims the top of the locals stack for itself
   Local* local = &current->locals[current->localCount++];
@@ -236,6 +246,8 @@ static ObjFunction* endCompiler() {
     }
   #endif
 
+  // when we end a compiler, go "back" a scope and reset current to the current's enclosing
+  current = current->enclosing;
   return function;
 }
 
@@ -261,6 +273,7 @@ static void endScope() {
 static void expression();
 static void statement();
 static void declaration();
+static void funDeclaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence) {
   // read the next token
@@ -361,6 +374,10 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+  // if a top level function is declared, do nothing since no local variable
+  if (current->scopeDepth == 0) return;
+
+  // the last local added should have its depth set to the current depth
   current->locals[current->localCount - 1].depth =
       current->scopeDepth;
 }
@@ -625,7 +642,9 @@ static void synchronize() {
 }
 
 static void declaration() {
-  if (match(TOKEN_VAR)) {
+  if (match(TOKEN_FUN)) {
+    funDeclaration();
+  } else if (match(TOKEN_VAR)) {
     varDeclaration();
   } else {
     statement();
@@ -640,6 +659,45 @@ static void block() {
   }
 
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
+static void function(FunctionType type) {
+  // each function gets its own inner compiler to generate it
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope();
+
+  // parameters
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+  if (!check(TOKEN_RIGHT_PAREN)) {
+    do {
+      current->function->arity++;
+      if (current->function->arity > 255) {
+        errorAtCurrent("Cannot have more than 255 parameters");
+      }
+
+      uint8_t paramConstant = parseVariable("Expect parameter name");
+      defineVariable(paramConstant);
+    } while (match(TOKEN_COMMA));
+  }
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+
+  // body
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block();
+
+  // create function object
+  ObjFunction* function = endCompiler();
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration() {
+  // if we see keyword fun, next token must be function name to declare
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+
+  function(TYPE_FUNCTION);
+  defineVariable(global);
 }
 
 static void expressionStatement() {
